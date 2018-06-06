e11ed5303eb3d80905292d5071d0b8ffa01fb1a8
Sara Chipps <sarajchipps@gmail.com>
2018-06-05 21:25:55 -0400

    lesson 1 commit

--- src/app/_models/model.ts
+++ src/app/_models/model.ts
@@ -1,18 +1,10 @@
 
-
-export class Card {
-        id: number;
-        text: string;
-        }
-
 export class List {
   id: number;
   title: string;
-  cards: Card[];
 }
 
 export class Board {
       id: number;
       name: string;
-      lists: List[];
 }
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Board } from '../_models/model';
 import { List } from '../_models/model';
-import { Card } from '../_models/model';
 import { Routes, ActivatedRoute } from '@angular/router';
-import { BoardService } from '../_services/board.service';
 
 import * as $ from 'jquery';
 
@@ -13,35 +11,11 @@ import * as $ from 'jquery';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-  board = new Board();
-  list = new List();
-  lists: List[];
-  submitted = false;
+  board: Board = {
+    id: 1,
+    name: "Personal Board"
+  };
+  constructor() {}
 
-  constructor(private route: ActivatedRoute, private boardService: BoardService) { }
-
-  ngOnInit() {
-     this.route.params.subscribe(params => {
-     this.boardService.getBoardById(params['id'])
-       .subscribe(board => {
-       console.log(board);
-       this.board = board;
-       this.lists = board['lists'];
-      });
-   });
-
-  $('#body').css('background-color', 'rgb(0, 121, 191)');
-   console.log($('#body'));
-  }
-
-submitForm = (boardId, listForm) => {
-    this.submitted = true;
-    console.log('here is the list', listForm);
-    const new_list = new List();
-    new_list.title = listForm.controls['title'].value;
-    this.boardService.addList(boardId, new_list as List)
-    .subscribe(list => {
-      this.lists.push(list);
-    });
-  }
+  ngOnInit() {}
 }
